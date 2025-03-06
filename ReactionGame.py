e5a5bd7f1bad50efb8ed803c57dba311a94dcfec
epeterson02 <157244412+epeterson02@users.noreply.github.com>
2025-03-06 15:01:24 -0600

    buttons work

--- ReactionGame.py
+++ ReactionGame.py
@@ -3,9 +3,16 @@ from time import sleep
 import random
 
 led = LED(18)
+right_button = Button(4)
+left_button = Button(17)
+player1 = "name"
+player2 = "name2"
 
-for i in range (0, 10):
-    led.on()
-    sleep(1)
-    led.off()
-    sleep(1)
+def pressed(button):
+    if button.pin.number ==4:
+        print(player1 + ' pressed first!')
+    elif button.pin.number ==17:
+        print(player2 + ' pressed first!')
+right_button.when_pressed = pressed
+left_button.when_pressed = pressed
+
